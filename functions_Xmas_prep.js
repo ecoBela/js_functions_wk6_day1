4a17bd0a726ae006553d832557c1ac996a430441
Bela <belahamid@gmail.com>
2021-01-04 18:13:56 +0000

    initial commit

---
+++ functions_Xmas_prep.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+// function sayHello(greeting, name = 'World'){
+//     return `${greeting} ${name}`;
+// }
+
+// console.log(sayHello("hi", "Trey"));
+
+// const add = function (firstNumber, secondNumber){
+//     return firstNumber + secondNumber;
+// }
+// console.log('1 + 3 with add: ', add(1, 3));
